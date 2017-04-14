d13166d6b5673855bc53dc8d32efa045019aeb89
cySheng <cyusheng93@gmail.com>
2017-04-14 14:16:01 +0800

    Added & install bcrypts gem

--- db/migrate/20170414060624_users.rb
+++ db/migrate/20170414060624_users.rb
@@ -1,4 +1,10 @@
 class Users < ActiveRecord::Migration[5.0]
   def change
+		# create_table :users do |t|
+		#   t.string :email
+		#   t.string :last_name 
+		#   t.string :first_name
+		#   t.string 
+		# end
   end
 end
