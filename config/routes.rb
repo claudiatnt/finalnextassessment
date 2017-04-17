f380603f1376f10e9d2f7ae22b257131af69bdb0
cySheng <cyusheng93@gmail.com>
2017-04-17 15:31:04 +0800

    Added omniauth initializers, added a route for callback of omniauth and create_from_omniauth method in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resource :session, only: [:create, :destroy, :new]
 
   root 'users#show'
+
+  get '/auth/:provider/callback', to: 'sessions#create_from_omniauth'
 end
