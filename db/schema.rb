65392c8c2f68457436852946f44ffbe4ac779ba7
cySheng <cyusheng93@gmail.com>
2017-04-19 12:42:42 +0800

    Added the user referenceing id to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20170419035124) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 20170419035124) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "posts", "users"
 end
