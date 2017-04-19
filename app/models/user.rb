8dd19500d86a43180bb524b1f2c9a6815ab39f01
cySheng <cyusheng93@gmail.com>
2017-04-19 12:36:02 +0800

    Added association between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
   has_many :authentications, :dependent => :destroy
+  has_many :posts
 	validates :last_name, presence: true
 	validates :first_name, presence: true
 	validates :email, presence: true, uniqueness: true, format: { with: /.+@.+./, message: "Please input valid email" }
