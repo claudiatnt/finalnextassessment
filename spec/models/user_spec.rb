e16bba966a0b14b93646dcaa26095d8b2dd1ee81
cySheng <cyusheng93@gmail.com>
2017-04-20 21:05:11 +0800

    before i change the whole file of rails helper in specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,37 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
-  	it "should have email, first name, last name and avatar" do
-  		byebug
-      should have_db_column(:email).of_type(:string)
-      should have_db_column(:first_name).of_type(:string)
-      should have_db_column(:last_name).of_type(:string)
-      should have_db_column(:avatar).of_type(:string)
-    end
+  	subject { described_class.new(password: "123456", first_name: "Sheng", last_name: "Chang", email: "cyusheng93@gmail.com") }
 
-    describe "validates presence of attributes" do
-      it { is_expected.to validate_presence_of(:last_name) }
+  	it "is valid with valid attributes" do
+    	expect(subject).to be_valid
+  	end
+
+  	it "is not valid without first name" do
+  		subject.first_name = nil
+  		expect(subject).to_not be_valid
+  	end
+
+  	it "is not valid without email" do
+  		subject.email = nil
+  		expect(subject).to_not be_valid
+  	end
+
+  	it "is not valid without last_name" do
+  		subject.last_name = nil
+  		expect(subject).to_not be_valid
+  	end
+
+  	it "is not valid without password" do
+  		subject.password = nil
+  		expect(subject).to_not be_valid
+  	end
+
+
+  	describe "validates presence of attributes" do
       it { is_expected.to validate_presence_of(:first_name) }
-      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:last_name) }
     end
   end
+
 end
