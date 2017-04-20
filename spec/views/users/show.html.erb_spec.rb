20fb1d456c63792e70a9c4e3c72cfdf93cd8a274
cySheng <cyusheng93@gmail.com>
2017-04-20 23:14:55 +0800

    Added the 2 different rspec test for integration

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 RSpec.describe "users/show.html.erb", type: :view do
 
 
-  context "logged in as person's profile" do
-    it "display edit out button" do
+  context "logged in as the person on the profile" do
+    it "display edit button" do
       user = FactoryGirl.create(:user)
       session[:user_id] = user.id
       @user = User.find(user.id)
       render
       expect(rendered).to have_link("Edit", href: edit_user_path(user.id))
     end
+
+    it "displays the user's name" do
+    	user = FactoryGirl.create(:user)
+    	session[:user_id] = user.id
+    	@user = User.find(user.id)
+    	render
+    	expect(rendered).to have_content(@user.name)
+    end
   end
+
+  # context "not logged in" do
+  # 	it "displays the new user" do
+  # 		render 
+  # 		expect(rendered.to have_link("Logout", href: session_path))
+  # 	end
+  # end
 
 end
 
