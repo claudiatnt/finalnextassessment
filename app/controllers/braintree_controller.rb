ecd3b803bd770a76237ae8b093efe39e97debac7
cySheng <cyusheng93@gmail.com>
2017-04-20 23:26:04 +0800

    Added migration file for users money and attempt to connect braintree to users money

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -6,8 +6,10 @@ class BraintreeController < ApplicationController
 	def checkout
 	  nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
 
+	  value = 5 
+
 	  result = Braintree::Transaction.sale(
-	   :amount => "10.00", #this is currently hardcoded
+	   :amount => value, #this is currently hardcoded
 	   :payment_method_nonce => nonce_from_the_client,
 	   :options => {
 	      :submit_for_settlement => true
@@ -15,7 +17,9 @@ class BraintreeController < ApplicationController
 	   )
 
 	  if result.success?
-	    redirect_to :root, :flash => { :success => "Transaction successful!" }
+	  	byebug
+	  	current_user.update(money + value)
+	    redirect_to :root, :flash => { :success => "Transaction successful! 5 dollars added!" }
 	  else
 	    redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
 	  end 
